1646e95924e182c68df014d5dda1d5ad88c8cf29
Nitin Singh <i.nitinsingh18@gmail.com>
2022-09-25 20:57:28 +0530

    Commit

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,20 +1,27 @@
 import 'package:get/get.dart';
 
-import 'package:tech_fest_admin_app/app/modules/home/bindings/home_binding.dart';
-import 'package:tech_fest_admin_app/app/modules/home/views/home_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/signin_screen/bindings/signin_screen_binding.dart';
+import '../modules/signin_screen/views/signin_screen_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.SIGNIN_SCREEN;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SIGNIN_SCREEN,
+      page: () => SigninScreenView(),
+      binding: SigninScreenBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,8 +5,10 @@ abstract class Routes {
   Routes._();
 
   static const HOME = _Paths.HOME;
+  static const SIGNIN_SCREEN = _Paths.SIGNIN_SCREEN;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
+  static const SIGNIN_SCREEN = '/signin-screen';
 }
