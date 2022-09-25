9f02edd877fc8223d2c7910f9e22a0d229fd8fae
3Divyanshu <64863512+3Divyanshu@users.noreply.github.com>
2022-09-25 22:42:03 +0530

    pu

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,16 @@ import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+    ScreenUtilInit(
+      designSize: const Size(360, 690),
+      builder: (context, widget) {
+        return GetMaterialApp(
+          title: "Application",
+          debugShowCheckedModeBanner: false,
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        );
+      },
     ),
   );
 }
