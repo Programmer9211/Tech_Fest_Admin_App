5df250429e4d308feec848a39c02a552bfabdf0d
Nitin Singh <i.nitinsingh18@gmail.com>
2022-09-25 20:57:41 +0530

    Login Screen

---
+++ lib/app/modules/signin_screen/controllers/signin_screen_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SigninScreenController extends GetxController {
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+}
---
+++ lib/app/modules/signin_screen/views/signin_screen_view.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:get/get.dart';
+
+import '../../../../const/app_const/app_color.dart';
+import '../../../data/widgets/custom_button.dart';
+import '../../../data/widgets/custom_textfield.dart';
+import '../controllers/signin_screen_controller.dart';
+
+class SigninScreenView extends GetView<SigninScreenController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffFAFAFA),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 22.w),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 125.h,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 2.w),
+                        child: Text(
+                          "Organiser",
+                          style: TextStyle(
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.w600,
+                            color: AppColor.black80,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "LOGIN",
+                        style: TextStyle(
+                          fontSize: 52.sp,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Image.asset("assets/icon.png"),
+                ],
+              ),
+              SizedBox(
+                height: 80.h,
+              ),
+              KTextField(
+                hintText: "Enter Your Email",
+                controller: controller.email,
+              ),
+              SizedBox(
+                height: 26.h,
+              ),
+              KTextField(
+                hintText: "Enter Password",
+                controller: controller.password,
+              ),
+              SizedBox(
+                height: 18.h,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                        color: AppColor.textColor, fontWeight: FontWeight.w500),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 18.h,
+              ),
+              KButton(title: "LOGIN", onTap: () {}),
+              SizedBox(
+                height: 140.h,
+              ),
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.w500),
+                  children: [
+                    TextSpan(text: "New User? "),
+                    TextSpan(
+                        text: "Register Here",
+                        style: TextStyle(color: AppColor.textColor))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
